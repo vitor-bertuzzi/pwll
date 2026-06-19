c0eccf6cbe436a96b53ed8c55687c052392abaed
Vítor Bertuzzi <vitorbertuzzi18@gmail.com>
2026-06-19 20:02:36 -0300

    exercicio 5 feito

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //nome: Vítor Bertuzzi Beltrami Turma: 2ºDS AMS
+//fiz até o exercicio 5, pois tive muita dificuldade para fazer os outros
 const { log } = require('console');
 const http = require('http');
 const { parse } = require('url');
 const alunos = [{id: 1, nome: 'Joao', turma: 1, curso: 'ds'},
     {id: 2, nome: 'Pedro', turma: 1, curso: 'mec'},
     {id: 3, nome: 'Vitor', turma: 3, curso: 'adm'},
-    {id: 4, nome: 'Lauro', turma: 2, curso: 'ds'},
+    {id: 4, nome: 'Enzo', turma: 3, curso: 'ds'},
     {id: 5, nome: 'Hugo', turma: 2, curso: 'logistica'}
 ];
 const produtos = [{id:1, nome:"Arroz 2kg", categoria:"alimento", preco:12.0,},
@@ -14,19 +15,19 @@ const produtos = [{id:1, nome:"Arroz 2kg", categoria:"alimento", preco:12.0,},
 {id:4, nome:"detergente", categoria:"higiene", preco:4.0,},
 {id:5, nome:"prato", categoria:"cozinha", preco:20.0,},
 ];
-const server = http.createServer((req, res)=>{
-    if(req.method === 'GET' && req.url === '/'){
+const server = http.createServer((req, res)=&gt;{
+    if(req.method === 'GET' &amp;&amp; req.url === '/'){
         res.writeHead(200,{'content-type': 'application/json'});
         res.end(JSON.stringify({mensagem: 'Bem vindo' }));
-    }else if(req.method === 'GET' && req.url === '/sobre'){
+    }else if(req.method === 'GET' &amp;&amp; req.url === '/sobre'){
         res.writeHead(200,{'content-type': 'application/json'});
         res.end(JSON.stringify({mensagem: 'olá esse é o /sobre' }));
-    }else if(req.method === 'GET' && req.url === '/status'){
+    }else if(req.method === 'GET' &amp;&amp; req.url === '/status'){
         res.writeHead(200,{'content-type': 'application/json'});
         res.end(JSON.stringify({mensagem: 'olá pode ver o status' }));
-    }else if(req.method === 'GET' && req.url.startsWith('/alunos/')){
+    }else if(req.method === 'GET' &amp;&amp; req.url.startsWith('/alunos/')){
         const id = Number(req.url.split("/")[2]);
-        const alunoencontrado = alunos.find(aluno => aluno.id === id);
+        const alunoencontrado = alunos.find(aluno =&gt; aluno.id === id);
         if(alunoencontrado){
         res.writeHead(200,{'content-type': 'application/json'});
         res.end(JSON.stringify(alunoencontrado));
@@ -34,10 +35,10 @@ const server = http.createServer((req, res)=>{
             res.writeHead(404,{'content-type': 'application/json'});
         res.end(JSON.stringify("ERROR 404 aluno nao encontrado"));
         }
-    }else if (req.method === 'GET' && req.url === '/alunos'){
+    }else if (req.method === 'GET' &amp;&amp; req.url === '/alunos'){
         res.writeHead(200,{'content-type': 'application/json'});
         res.end(JSON.stringify(alunos));
-    } else if (req.method === 'GET' && req.url.startsWith('/produtos')) {
+    } else if (req.method === 'GET' &amp;&amp; req.url.startsWith('/produtos')) {
         res.writeHead(200, { 'content-type': 'application/json' });
         const minhaurl = new URL(req.url, `http://${req.headers.host}`);
         const filtrarcategoria = minhaurl.searchParams.get('categoria');
@@ -45,42 +46,49 @@ const server = http.createServer((req, res)=>{
         let Enviarprodutos = produtos; 
 
         if (filtrarcategoria) {
-            Enviarprodutos = produtos.filter(produto => produto.categoria === filtrarcategoria);
+            Enviarprodutos = produtos.filter(produto =&gt; produto.categoria === filtrarcategoria);
         }
 
         res.end(JSON.stringify(Enviarprodutos));
-    }else if (req.method === 'POST' && req.url === '/alunos') {
+    }else if (req.method === 'POST' &amp;&amp; req.url === '/alunos') {
         let body = '';
 
-        req.on('data', chunk => {
-            body = chunk.toString();   
+        req.on('data', chunk =&gt; {
+            body += chunk.toString();   
         });
-
-        req.on('end', () => {
+            
+        req.on('end', () =&gt; {
             try {
                 const novoAluno = JSON.parse(body);
-                // 2. Validar 'nome' e 'turma'
+                const ids = alunos.map(aluno =&gt; aluno.id);
+                let novoId = 1;
                 if(!novoAluno.nome || !novoAluno.turma){
-
-                }else{
                     res.writeHead(400,{'content-type': 'application/json'});
-                    res.end(JSON.stringify(mensagem="400 requisição não encontrada"));
-                    return;
-                }
-                alunos.map();
-                Math.max();
-                const ids = alunos.map(aluno => aluno.id);
+                    res.end(JSON.stringify({mensagem:"400 requisição não encontrada"}));
+                     return;
+                }else{
+                    
+                    }if(alunos.length === 0){ 
+                        novoAluno.id = novoId;
+                        alunos.push(novoAluno);
+
+                    }else{
+                        novoId = Math.max(...ids) + 1;
+                        novoAluno.id = novoId;
+                        alunos.push(novoAluno);                  
+                    }
+                    res.writeHead(201,{'content-type': 'application/json'});
+                    res.end(JSON.stringify(novoAluno));
             } catch (error) {
                res.writeHead(400,{'content-type': 'application/json'});
-              res.end(JSON.stringify(mensagem="400 requisição não encontrada"));
+              res.end(JSON.stringify({mensagem:"400 requisição não encontrada"}));
+              return;
             }
-             res.writeHead(201,{'content-type': 'application/json'});
-            res.end(JSON.stringify(novoAluno));
+             
         });
     }
 });
 
-
-server.listen(3000, () =>{
+server.listen(3000, () =&gt;{
     console.log('http://localhost:3000/')
 });
